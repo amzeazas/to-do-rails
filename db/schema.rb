4e5587e9c4d32b883d26671773e8c347ed3a4ce4
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-10-12 10:45:01 -0700

    Add tasks table in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012161953) do
+ActiveRecord::Schema.define(version: 20151012174323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "list_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
